9b671da507e114cd3e4118af418eeda4bdcce196
Alida Anna Thomas <alidaannathomas@gmail.com>
2024-10-23 19:27:39 +0530

    updated doctor

--- updatedoctor.php
+++ updatedoctor.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    $query = "SELECT * FROM doctor WHERE did='$id'";
+    $query = "SELECT u.firstname,u.lastname,u.phno,u.email,d.qualification,d.specialization,d.Date_of_joining FROM doctor_details d inner join user u on  u.uid=d.uid";
     $result = mysqli_query($con, $query);
 
     if ($result && mysqli_num_rows($result) > 0) {
@@ -18,17 +18,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 if (isset($_POST["submit"])) {
-    $n = $_POST["name"];
+    $fn = $_POST["firstname"];
+    $ln = $_POST["lastname"];
     $phn = $_POST["phno"];
     $q = $_POST["qua"];
     $s = $_POST["spe"];
     $doj = $_POST["doj"];
     $email = $_POST["email"];
-    $u = $_POST["un"];
 
-    $query2 = "UPDATE doctor SET name='$n', qualification='$q', DOJ='$doj', specialization='$s', username='$u', phno='$phn', email='$email' WHERE did='$id'";
+    $query1 = "UPDATE doctor_details SET  qualification='$q', Date_of_joining='$doj', specialization='$s' WHERE uid='$id'";
+    $query2 = "UPDATE user SET firstname='$fn',lastname='$ln',phno='$phn', email='$email' WHERE uid='$id'";
+    $result1 = mysqli_query($con, $query1);
     $result2 = mysqli_query($con, $query2);
-    if ($result2) {
+    if ($result1 && $result2) {
       echo "Record updated successfully.";
   } else {
       echo "Error updating record: " . mysqli_error($con);
@@ -51,13 +53,13 @@ if (isset($_POST["submit"])) {
 <div class="container mt-3">
     <form action="updatedoctor.php" method="POST">
         <input type="hidden" name="id" value="<?php echo isset($id) ? $id : ''; ?>"><br><br>
-        Name:<input type="text" class="form-control mt-3" name="name" value="<?php echo isset($row['name']) ? $row['name'] : ''; ?>"><br><br>
+        FirstName:<input type="text" class="form-control mt-3" name="firstname" value="<?php echo isset($row['firstname']) ? $row['firstname'] : ''; ?>"><br><br>
+        LastName:<input type="text" class="form-control mt-3" name="lastname" value="<?php echo isset($row['lastname']) ? $row['lastname'] : ''; ?>"><br><br>
         PhoneNumber:<input type="number" class="form-control mt-3" name="phno" value="<?php echo isset($row['phno']) ? $row['phno'] : ''; ?>"><br><br>
         Qualification:<input type="text" class="form-control mt-3" name="qua" value="<?php echo isset($row['qualification']) ? $row['qualification'] : ''; ?>"><br><br>
         Specialization:<input type="text" class="form-control mt-3" name="spe" value="<?php echo isset($row['specialization']) ? $row['specialization'] : ''; ?>"><br><br>
-        Date-Of-Joining:<input type="date" name="doj" class="form-control mt-3" value="<?php echo isset($row['DOJ']) ? $row['DOJ'] : ''; ?>"><br><br>
+        Date-Of-Joining:<input type="date" name="doj" class="form-control mt-3" value="<?php echo isset($row['Date_of_joining']) ? $row['Date_of_joining'] : ''; ?>"><br><br>
         Email:<input type="email" name="email" class="form-control mt-3" value="<?php echo isset($row['email']) ? $row['email'] : ''; ?>"><br><br>
-        Username:<input type="text" name="un" class="form-control mt-3" value="<?php echo isset($row['username']) ? $row['username'] : ''; ?>"><br><br>
         <input type="submit" class="btn btn-primary" name="submit" value="Update">
     </form>
 </div>
