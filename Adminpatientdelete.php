99978abe642323b00eb2c1120d0fe323e6b109fe
Alida Thomas <alidathomas@Alidas-MacBook-Air.local>
2024-10-31 11:48:00 -0700

    added

--- Adminpatientdelete.php
+++ Adminpatientdelete.php
@@ -1,16 +1,24 @@
 <?php
 include 'conn.php';
-if($_SERVER['REQUEST_METHOD']=== 'POST')
-{
-    $id=$_POST["id"];
-    $query="delete from user where uid='$id'";
-    if(mysqli_query($con,$query))
-    {
-        header("Location:patientdetails.php");
-        exit();
-    }
-    else{
-        echo "Error deleting record: " . mysqli_error($con);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $un = $_SESSION['username']; 
+    $query = "SELECT uid FROM login WHERE username='$un'";
+    $result = mysqli_query($con, $query);
+
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
+        $id = $row['uid']; 
+
+        $query = "DELETE FROM user WHERE uid='$id'";
+        if (mysqli_query($con, $query)) {
+            header("Location: Adminpatientdetails.php");
+            exit();
+        } else {
+            echo "Error deleting record: " . mysqli_error($con);
+        }
+    } else {
+        echo "Error fetching user ID: " . mysqli_error($con);
     }
 }
 ?>
