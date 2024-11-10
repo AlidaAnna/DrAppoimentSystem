242accb38f36ea0f61f470e63674d5ea356b510d
deepuezhap <deepuezhap007@gmail.com>
2024-11-10 21:45:04 +0530

    navbar for patient details to

--- admin.php
+++ admin.php
@@ -45,7 +45,7 @@ if (!isset($_SESSION['username'])) {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link active" aria-current="page" href="admin.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="Adminpatientdetails.php">Patient</a>
